e2988b60ed32316d60201b59e4e0ca383c894785
evanblaine <evanblaineadams@gmail.com>
2020-11-14 19:17:38 -0600

    version 0.0.2

--- randw/words.py
+++ randw/words.py
@@ -4,7 +4,7 @@ from os.path import abspath, join, dirname
 __title__ = 'randw'
 __license__ = 'MIT'
 __author__ = 'Evan Adams'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 path = lambda fname: abspath(join(dirname(__file__), fname))
 
@@ -18,7 +18,13 @@ def get_words(fname) -> str:
     return line.strip()
 
 def get_adj():
+    """PARAMS:
+        NONE
+    """
     return get_words(F['adj'])
 
 def get_noun():
+    """PARAMS:
+        NONE
+    """
     return get_words(F['noun'])
