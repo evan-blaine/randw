be0c3fca435ad635aba80623e540899602f8e01f
evanblaine <evanblaineadams@gmail.com>
2020-11-15 14:03:07 -0600

    version 0.0.3
    Updated README.md and words.py

--- randw/words.py
+++ randw/words.py
@@ -4,7 +4,7 @@ from os.path import abspath, join, dirname
 __title__ = 'randw'
 __license__ = 'MIT'
 __author__ = 'Evan Adams'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 path = lambda fname: abspath(join(dirname(__file__), fname))
 
@@ -17,14 +17,16 @@ def get_words(fname) -> str:
     line = random.choice(open(fname).readlines())
     return line.strip()
 
-def get_adj():
-    """PARAMS:
-        NONE
+def adj():
+    """
+    PARAMS:
+    NONE
     """
     return get_words(F['adj'])
 
-def get_noun():
-    """PARAMS:
-        NONE
+def noun():
+    """
+    PARAMS:
+    NONE
     """
     return get_words(F['noun'])
